9532f5a602b3e7725e5abbeadad2eb37947ed16b
AHAMAD ASHIQ <ahamadashiq95@gmail.com>
2025-07-15 21:19:47 +0530

    Final

--- freegamedownload/management/commands/create_admin.py
+++ freegamedownload/management/commands/create_admin.py
@@ -1,13 +1,18 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
+import os
 
 class Command(BaseCommand):
-    help = 'Creates a superuser if none exist'
+    help = 'Creates an admin user'
 
     def handle(self, *args, **kwargs):
         User = get_user_model()
-        if not User.objects.filter(is_superuser=True).exists():
-            User.objects.create_superuser('ibraz', 'tpinto330@gmail.com', 'Adminibraz@123')
-            self.stdout.write(self.style.SUCCESS('Superuser created successfully!'))
+        username = os.getenv('DJANGO_ADMIN_USERNAME', 'admin')
+        email = os.getenv('DJANGO_ADMIN_EMAIL', 'admin@example.com')
+        password = os.getenv('DJANGO_ADMIN_PASSWORD', 'admin123')
+
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(username=username, email=email, password=password)
+            self.stdout.write(self.style.SUCCESS(f"Superuser '{username}' created successfully."))
         else:
-            self.stdout.write(self.style.WARNING('Superuser already exists.'))
+            self.stdout.write(self.style.WARNING(f"Superuser '{username}' already exists."))
