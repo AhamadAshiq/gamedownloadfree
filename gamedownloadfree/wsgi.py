ebf689b7a8be0f779f8e7d955f34d1d1db3d68de
AHAMAD ASHIQ <ahamadashiq95@gmail.com>
2025-07-17 13:10:21 +0530

    Final

--- gamedownloadfree/wsgi.py
+++ gamedownloadfree/wsgi.py
@@ -15,19 +15,3 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gamedownloadfree.settings')
 
 application = get_wsgi_application()
 
-import os
-from django.contrib.auth import get_user_model
-
-def create_superuser():
-    User = get_user_model()
-    username = os.getenv("DJANGO_ADMIN_USERNAME", "admin")
-    email = os.getenv("DJANGO_ADMIN_EMAIL", "admin@example.com")
-    password = os.getenv("DJANGO_ADMIN_PASSWORD", "admin123")
-
-    if not User.objects.filter(username=username).exists():
-        print("Creating superuser...")
-        User.objects.create_superuser(username=username, email=email, password=password)
-    else:
-        print("Superuser already exists.")
-
-create_superuser()
